71581931dba6936017a3a9fb90a4e040289c0799
Nicky Stevens <lnickystevens@gmail.com>
2024-08-29 19:10:11 -0400

    Rendered the definitions and examples

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,32 +1,36 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState(null);
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for the definition of ${keyword}`);
+
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
-  //documentation: https://www.shecodes.io/learn/apis/dictionary
-  let apiKey = "fb24932f22a0f4a4b10t5cee3cc7fo79";
-  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-  axios.get(apiUrl).then(handleResponse);
+  //documentation: https://dictionaryapi.dev/
 
   function keywordChange(event) {
     setKeyword(event.target.value);
   }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={keywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
