abd80264ac1c4c3cc06c3fe164ebbd33fb55f745
Nicky Stevens <lnickystevens@gmail.com>
2024-08-29 11:04:02 -0400

    Added a Dictionary component and form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">📖</header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">Coded by Nicky Stevens</footer>
     </div>
   );
 }
