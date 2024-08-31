504e4deb6080235035c4a353aaf50606348e0a93
Nicky Stevens <lnickystevens@gmail.com>
2024-08-30 21:27:47 -0400

    Updated defaultKeyword and removed paragraph creating warning

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1>The Dictionary App</h1>
       </header>
       <main>
-        <Dictionary defaultKeyword="highlands" />
+        <Dictionary defaultKeyword="flying" />
       </main>
       <footer className="App-footer">
         Coded by{" "}
@@ -26,8 +26,8 @@ function App() {
           rel="noopener noreferrer"
         >
           GitHub
-        </a>{" "}
-        and hosted on{" "}
+        </a>
+        , and hosted on{" "}
         <a
           href="https://dictionary-nicky-stevens.netlify.app/"
           target="_blank"
--- src/Meaning.js
+++ src/Meaning.js
@@ -6,11 +6,9 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>
-        <div className="definition">{props.meaning.definition}</div>
-        <div className="example">{props.meaning.example}</div>
-        <Synonyms synonyms={props.meaning.synonyms} />
-      </p>
+      <div className="definition">{props.meaning.definition}</div>
+      <div className="example">{props.meaning.example}</div>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
